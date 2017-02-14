fa3c5b3132a651f629752aa65cbf4e5a905353aa
Paul <paul.delicano@mobileoptima.com>
2017-02-14 12:07:39 +0800

    Paul

--- app/src/main/java/com/mobileoptima/tarkieform/LoginFragment.java
+++ app/src/main/java/com/mobileoptima/tarkieform/LoginFragment.java
@@ -16,39 +16,41 @@ import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
 import com.codepan.utils.CodePanUtils;
+import com.codepan.widget.CodePanButton;
+import com.codepan.widget.CodePanTextField;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 public class LoginFragment extends Fragment implements OnClickListener {
-	private EditText etUsernameLogin, etPasswordLogin;
+
+	private CodePanButton btnLogin;
+	private CodePanTextField etUsernameLogin, etPasswordLogin;
+	private DisplayImageOptions options;
 	private ImageLoader imageLoader;
-	private TextView btnLogin;
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getActivity().setTheme(android.R.style.Theme);
 		imageLoader = ImageLoader.getInstance();
 		imageLoader.init(ImageLoaderConfiguration.createDefault(getActivity()));
+		options = new DisplayImageOptions.Builder()
+				.showImageOnLoading(R.drawable.ic_logo)
+				.showImageForEmptyUri(R.drawable.ic_logo)
+				.cacheInMemory(true)
+				.cacheOnDisk(true)
+				.build();
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.login_layout, container, false);
-		view.findViewById(R.id.rlMain).setOnClickListener(this);
 		ImageView ivCompanyLogo = (ImageView) view.findViewById(R.id.ivLogoAuthorization);
-		DisplayImageOptions options = new DisplayImageOptions.Builder()
-				.showStubImage(R.drawable.ic_logo)
-				.showImageForEmptyUri(R.drawable.ic_logo)
-				.cacheInMemory()
-				.cacheOnDisc()
-				.build();
 		imageLoader.displayImage("https://hatscripts.com/r/03d6.png", ivCompanyLogo, options);
-		etUsernameLogin = (EditText) view.findViewById(R.id.etUsernameLogin);
-		etPasswordLogin = (EditText) view.findViewById(R.id.etPasswordLogin);
+		etUsernameLogin = (CodePanTextField) view.findViewById(R.id.etUsernameLogin);
+		etPasswordLogin = (CodePanTextField) view.findViewById(R.id.etPasswordLogin);
 		etPasswordLogin.setTransformationMethod(new PasswordTransformationMethod());
-		btnLogin = (TextView) view.findViewById(R.id.btnAuthorization);
+		btnLogin = (CodePanButton) view.findViewById(R.id.btnAuthorization);
 		btnLogin.setOnClickListener(this);
 		etPasswordLogin.setOnEditorActionListener(new OnEditorActionListener() {
 			@Override
@@ -72,7 +74,6 @@ public class LoginFragment extends Fragment implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		CodePanUtils.hideKeyboard(v, getActivity());
 		switch(v.getId()) {
 			case R.id.btnAuthorization:
 				String username = etUsernameLogin.getText().toString().trim();
