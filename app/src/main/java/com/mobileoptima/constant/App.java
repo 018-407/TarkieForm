4607ee939ba1c3ca9c9c1881f2a8577996574715
Jeffrey <jeffre.concha@mobileoptima.com>
2017-03-02 18:34:37 +0800

    Tarkie Form

--- app/src/main/java/com/mobileoptima/constant/App.java
+++ app/src/main/java/com/mobileoptima/constant/App.java
@@ -5,8 +5,8 @@ public class App {
 	public static final String DB = "tarkieform.db";
 	public static final String DB_PWD = null;
 	public static final int DB_VERSION = 1;
-	//public static final String WEB_URL = "http://192.168.254.115/";//Darren
-	public static final String WEB_URL = "http://192.168.254.109/";//Adrian
+	public static final String WEB_URL = "http://192.168.254.115/";//Darren
+	//public static final String WEB_URL = "http://192.168.254.109/";//Adrian
 	public static final String API_V10 = WEB_URL + "gotarkie/API/1.0/";
 	public static final String API_V11 = WEB_URL + "gotarkie/API/1.1/";
 	public static final String API_KEY = "sample";
