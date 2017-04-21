dd75c15367da57e07cbcedf1e866ab7e2be8f2cb
Jeffrey <jeffre.concha@mobileoptima.com>
2017-04-21 17:03:28 +0800

    Tarkie Form

--- app/src/main/java/com/mobileoptima/constant/App.java
+++ app/src/main/java/com/mobileoptima/constant/App.java
@@ -6,10 +6,9 @@ public class App {
 	public static final String DB_PWD = null;
 	public static final int DB_VERSION = 1;
 	//public static final String WEB_URL = "http://192.168.254.115/";//Darren
-	public static final String WEB_HOST = "http://form.local/";//Adrian
+	public static final String WEB_HOST = "http://www.getnoove.com/";//Adrian
 	public static final String API_V10 = WEB_HOST + "API/1.0/";
 	public static final String FILES_V10 = WEB_HOST + "FILES/1.0/";
-	public static final String API_V11 = WEB_HOST + "API/1.1/";
 	public static final String API_KEY = "V0gu1964h5j762s7WiG52i45CMg1s9Xo8dbX565P20m3w7U7CA";
 	public static final String OS_TYPE = "ANDROID";
 	public static final String FOLDER = "tarkieform";
